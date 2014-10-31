b202b063df8e25785433593bf92c778ef37a84fb
yoshuawuyts <i@yoshuawuyts.com>
2014-10-31 20:57:21 +0100

    test: assert xhr errors

--- test/xhr.js
+++ test/xhr.js
@@ -5,15 +5,29 @@
  */
 
 var proxyquire = require('proxyquire');
+var fax = proxyquire('../lib/index', {xhr: xhrStub, '@noCallThru': true});
+var errMsg = null;
+var err = false;
 
 /**
  * Mock `xhr()` in `fax()`.
  */
 
-var fax = proxyquire('../lib/index', {xhr: xhrStub, '@noCallThru': true});
 function xhrStub(opts, cb) {
-  console.log(cb());
+  if (err) {
+    try {
+      return cb('boom');
+    } catch(e) {
+      errMsg = e;
+    }
+  }
+  cb();
 }
+
+afterEach(function() {
+  err = false;
+  errMsg = null;
+});
 
 /**
  * Test
@@ -22,12 +36,25 @@ function xhrStub(opts, cb) {
 describe('xhr', function() {
   it('should perform an xhr request', function() {
     var app = fax();
+
     app.use(function *(next) {
       this.url = 'localhost:3000';
       yield next;
     });
+
     app.send();
   });
 
-  it('should handle xhr errors');
+  it('should handle xhr errors', function() {
+    var app = fax();
+    err = true;
+
+    app.use(function *(next) {
+      this.url = 'localhost:3000';
+      yield next;
+    });
+
+    app.send(app);
+    errMsg.message.should.eql('boom');
+  });
 });
