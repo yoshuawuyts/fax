de5aa8ea431f0c149fd0a4a84ea1fe912814dd0d
yoshuawuyts <i@yoshuawuyts.com>
2014-12-27 02:56:12 +0100

    test/url: mocha -> tap

--- test/request/url.js
+++ test/request/url.js
@@ -1,19 +1,10 @@
-/*eslint no-unused-expressions: 0*/
 
-/**
- * Module dependencies
- */
+const context = require('../context').context
+const test    = require('tape')
 
-var context = require('../context');
-
-/**
- * Test
- */
-
-describe('ctx.url()', function() {
-  it('should set an url', function() {
-    var ctx = context();
-    ctx.url = 'foo';
-    ctx.url.should.eql('foo');
-  });
-});
+test('ctx.url() should set an url', function(t) {
+  t.plan(1)
+  const ctx = context()
+  ctx.url = 'foo'
+  t.equal(ctx.url, 'foo')
+})
