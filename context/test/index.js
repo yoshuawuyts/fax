752b3bf5c45ab47d50de643662829a0b49500140
yoshuawuyts <i@yoshuawuyts.com>
2014-12-27 02:47:45 +0100

    test/onerror: mocha -> tap

---
+++ context/test/index.js
@@ -0,0 +1,2 @@
+require('./onerror')
+// require('./throw')
--- test/context/onerror.js
+++ test/context/onerror.js
@@ -1,57 +1,24 @@
-/**
- * Module dependencies.
- */
 
-var fax = require('../../')
+const context = require('../context').context
+const test    = require('tape')
 
-/**
- * Test.
- */
+test('ctx.onerror() should assert arg types', function(t) {
+  t.plan(1)
+  const ctx = context()
+  t.throws(ctx.onerror.bind(ctx, 'foo'), /non-error thrown/)
+})
 
-describe('ctx.onerror(err)', function() {
-  it('should assert argument types', function(done) {
-    var app = fax();
+test('ctx.onerror() should do nothing if no err is passed', function(t) {
+  t.plan(1)
+  const ctx = context()
+  t.equal(ctx.onerror(), undefined)
+})
 
-    app.use(function *() {
-      try {
-        this.onerror('foo');
-      } catch(e) {
-        e.message.should.eql('non-error thrown: foo');
-        done();
-      }
-    });
-
-    app.send();
-  });
-
-  it('should do nothing if no err is passed', function(done) {
-    var app = fax();
-    var called = false;
-
-    app.use(function *() {
-      try {
-        this.onerror();
-      } catch(e) {
-        called = true;
-      }
-      called.should.eql(false);
-      done();
-    });
-
-    app.send();
-  });
-
-  it('should emit an error', function(done) {
-    var app = fax();
-
-    app.on('error', function() {
-      done();
-    });
-
-    app.use(function *() {
-      this.onerror(new Error('boom'));
-    });
-
-    app.send();
-  });
-});
+test('ctx.onerror() should emit an error', function(t) {
+  t.plan(1)
+  const ctx = context()
+  ctx.app.on('error', function(err) {
+    t.ok(err.message, 'boom', 'err msg')
+  })
+  ctx.onerror(new Error('boom'))
+})
